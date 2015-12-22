cda0708977b9c25179326e002bcd1278642c12a0
Cornell Daly <cmdaly@stetson.edu>
2015-12-22 16:54:30 -0500

    Added ability to draw multiple fixtures attached to a Box2D body in the debug drawer.

--- TwinStickRoguelike/src/systems/PhysicsDebugDrawSystem.cpp
+++ TwinStickRoguelike/src/systems/PhysicsDebugDrawSystem.cpp
@@ -12,40 +12,44 @@ void PhysicsDebugDrawSystem::processEntity(ECS::Entity* p_entity, float p_dt)
   auto cPhysics = p_entity->get<PhysicsComponent>();
 
   auto body = cPhysics->body;
-  auto shape = body->GetFixtureList()->GetShape();
-
-  if (shape->GetType() == b2Shape::Type::e_circle)
-  {
-    auto radius = shape->m_radius;
-    radius *= Game::PIXELS_PER_METER;
-
-    sf::CircleShape circle;
-    circle.setRadius(radius);
-    circle.setOrigin(radius, radius);
-    circle.setPosition(body->GetPosition().x * Game::PIXELS_PER_METER, body->GetPosition().y * Game::PIXELS_PER_METER);
-    circle.setFillColor(sf::Color::Transparent);
-    circle.setOutlineColor(sf::Color::Red);
-    circle.setOutlineThickness(1);
-    m_renderTarget.draw(circle);
-  }
-  else if (shape->GetType() == b2Shape::Type::e_polygon)
-  {
-    auto boxPolygon = dynamic_cast<b2PolygonShape*>(shape);
-
-    sf::ConvexShape sfPolygon;
-    sfPolygon.setPosition(body->GetPosition().x * Game::PIXELS_PER_METER, body->GetPosition().y * Game::PIXELS_PER_METER);
-    sfPolygon.setFillColor(sf::Color::Transparent);
-    sfPolygon.setOutlineColor(sf::Color::Red);
-    sfPolygon.setOutlineThickness(1);
-
-    sfPolygon.setPointCount(boxPolygon->GetVertexCount());
-    for (auto i = 0; i < boxPolygon->GetVertexCount(); i++)
-    {
-      auto vertex = boxPolygon->GetVertex(i);
-
-      sfPolygon.setPoint(i, sf::Vector2f(vertex.x * Game::PIXELS_PER_METER, vertex.y * Game::PIXELS_PER_METER));
-    }
   
-    m_renderTarget.draw(sfPolygon);
+  for (auto fixture = body->GetFixtureList(); fixture; fixture = fixture->GetNext())
+  {
+    auto shape = fixture->GetShape();
+
+    if (shape->GetType() == b2Shape::Type::e_circle)
+    {
+      auto radius = shape->m_radius;
+      radius *= Game::PIXELS_PER_METER;
+
+      sf::CircleShape circle;
+      circle.setRadius(radius);
+      circle.setOrigin(radius, radius);
+      circle.setPosition(body->GetPosition().x * Game::PIXELS_PER_METER, body->GetPosition().y * Game::PIXELS_PER_METER);
+      circle.setFillColor(sf::Color::Transparent);
+      circle.setOutlineColor(sf::Color::Red);
+      circle.setOutlineThickness(1);
+      m_renderTarget.draw(circle);
+    }
+    else if (shape->GetType() == b2Shape::Type::e_polygon)
+    {
+      auto boxPolygon = dynamic_cast<b2PolygonShape*>(shape);
+
+      sf::ConvexShape sfPolygon;
+      sfPolygon.setPosition(body->GetPosition().x * Game::PIXELS_PER_METER, body->GetPosition().y * Game::PIXELS_PER_METER);
+      sfPolygon.setFillColor(sf::Color::Transparent);
+      sfPolygon.setOutlineColor(sf::Color::Red);
+      sfPolygon.setOutlineThickness(1);
+
+      sfPolygon.setPointCount(boxPolygon->GetVertexCount());
+      for (auto i = 0; i < boxPolygon->GetVertexCount(); i++)
+      {
+        auto vertex = boxPolygon->GetVertex(i);
+
+        sfPolygon.setPoint(i, sf::Vector2f(vertex.x * Game::PIXELS_PER_METER, vertex.y * Game::PIXELS_PER_METER));
+      }
+
+      m_renderTarget.draw(sfPolygon);
+    }
   }
 }
