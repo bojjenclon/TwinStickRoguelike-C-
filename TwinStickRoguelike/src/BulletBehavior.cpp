f464b99758c9df677cdf00f2c9613e50f8aa9f29
Cornell Daly <cmdaly@stetson.edu>
2015-12-27 23:26:33 -0500

    Tweaked the basic bullet collision code to be a bit more robust.

--- TwinStickRoguelike/src/BulletBehavior.cpp
+++ TwinStickRoguelike/src/BulletBehavior.cpp
@@ -7,28 +7,38 @@
 
 void BulletBehavior::basicBulletCollision(CollisionData* p_dataA, CollisionData* p_dataB)
 {
-  auto bulletCollisionData = p_dataA->type == EntityInfo::Type::Bullet ? static_cast<BulletCollisionData*>(p_dataA) : static_cast<BulletCollisionData*>(p_dataB);
-  auto enemyCollisionData = p_dataA->type == EntityInfo::Type::Enemy ? p_dataA : p_dataB;
-
-  if (bulletCollisionData->owner == EntityInfo::Player)
+  // make sure that any enemy or the player is being hit, as they're all that matter for the basic bullet
+  if (p_dataA->type != EntityInfo::Type::Enemy && p_dataB->type != EntityInfo::Type::Enemy && p_dataA->type != EntityInfo::Type::Player && p_dataB->type != EntityInfo::Type::Player)
   {
-    auto cHitOnce = bulletCollisionData->entity->get<HitOnceComponent>();
+    return;
+  }
 
-    if (find(cHitOnce->alreadyHit.begin(), cHitOnce->alreadyHit.end(), enemyCollisionData->entity->getId()) == cHitOnce->alreadyHit.end())
+  auto bulletCollisionData = p_dataA->type == EntityInfo::Type::Bullet ? static_cast<BulletCollisionData*>(p_dataA) : static_cast<BulletCollisionData*>(p_dataB);
+  auto entityCollisionData = p_dataA->type == EntityInfo::Type::Enemy ? p_dataA : p_dataB;
+
+  // make sure the bullet doesn't affect its owner (based on type, so enemies can't hit each other)
+  if (bulletCollisionData->owner == entityCollisionData->type)
+  {
+    return;
+  }
+
+  auto cHitOnce = bulletCollisionData->entity->get<HitOnceComponent>();
+  if (find(cHitOnce->alreadyHit.begin(), cHitOnce->alreadyHit.end(), entityCollisionData->entity->getId()) == cHitOnce->alreadyHit.end())
+  {
+    auto cHealth = entityCollisionData->entity->get<HealthComponent>();
+    cHealth->currentHealth--;
+
+    cHitOnce->alreadyHit.push_back(entityCollisionData->entity->getId());
+
+    if (bulletCollisionData->owner == EntityInfo::Player && entityCollisionData->type == EntityInfo::Enemy)
     {
       auto& game = Game::Get();
-
-      game.setTarget(enemyCollisionData->entity);
-
-      auto cHealth = enemyCollisionData->entity->get<HealthComponent>();
-      cHealth->currentHealth--;
+      game.setTarget(entityCollisionData->entity);
 
       if (cHealth->currentHealth <= 0)
       {
         game.clearTarget();
       }
-
-      cHitOnce->alreadyHit.push_back(enemyCollisionData->entity->getId());
     }
   }
 }
